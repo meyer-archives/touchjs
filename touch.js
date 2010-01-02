e861adac01204a64d6a663075c8000d7038d1065
Mike Meyer (old) <github@meyer.fm>
2010-01-01 20:56:27 -0800

    MOAR

--- touch.js
+++ touch.js
@@ -1,45 +1,22 @@
-//// CONFIG
-
-function Finger(){
-	// Empty arrays
-	this.x = [],
-	this.y = []
+Array.prototype.max = function(){
+    return Math.max.apply( this );
+}
+Array.prototype.min = function(){
+    return Math.min.apply( this );
 }
 
-device = {
-	finger1: false,
-	finger2: false,
-	touch: {
-		count: 0,
-		start: 0,
-		duration: 0
+var log = {
+	add: function(msg){
+		x$("#content-inner ul").html("top",msg+"");
 	},
-	rotate: {
-		initial: 0, // initial rotation
-		prev: 0, // previous rotation
-		final: 0, // new rotation
-		delta: 0
-	},
-	pinch: {
-		initial: 0, // initial distance
-		final: 0, // final distance
-		delta: 0, // difference between the two
-		angle: 0 // angle at which the pinch is being performed
-	},
-	swipe: {
-		initial: {
-			x: 0,
-			y: 0
-		}, // initial distance, touch object
-		final: {
-			x: 0,
-			y: 0
-		}, // final distance, touch object
-		delta: 0, // swipe distance
-		angle: 0, // swipe angle // TODO: Round to the nearest X degrees?
-		direction: "" // human-readable version of the swipe angle: up, down, etc.
-	},
+	reset: function(){
+		x$("#content-inner ul").html("");
+	}
+}
 
+var device = {
+	touch: [],
+	// Read-only meta info
 	type: false,
 	orientation: false,
 	rotating: false,
@@ -49,7 +26,7 @@ device = {
 }
 
 // Nice-looking custom events
-event = {
+var event = {
 	// Some code I snarfed from here and modified : http://bit.ly/6s0rhu
 	fire: function(eventName) {
 		var e = document.createEvent("Events");
@@ -58,6 +35,64 @@ event = {
 	},
 	listen: function(eventName, functionToCall) {
 		window.addEventListener( eventName, functionToCall, false);
+	}
+}
+
+// Basic "finger" class
+function Finger(touchObject){
+	// Empty arrays
+	this.x = [];
+	this.y = [];
+	this.angle = []; // first angle can't be set, silly
+	this.id = touchObject.identifier;
+
+	var initX = touchObject.pageX;
+	var initY = touchObject.pageY;
+
+	this.addPoint = function(){
+		var prevX, prevY;
+
+		if( this.angle.length == 0 ){
+			prevX = initX;
+			prevY = initY;
+		} else {
+			prevX = this.x[this.x.length-1];
+			prevY = this.y[this.y.length-1];
+		}
+
+		var newX = touchObject.pageX;
+		var newY = touchObject.pageY;
+
+		var diffX = prevX-newX;
+		var diffY = prevY-newY;
+
+		var delta = Math.round( Math.sqrt( diffX*diffX + diffY*diffY ) );
+
+		if( delta > 10 ){
+
+			var angle = (360 - (Math.floor( ( Math.atan2(diffX,diffY)/Math.PI ) * 180 ))) % 360;
+
+			// Record a new point
+			this.x.push(newX);
+			this.y.push(newY);
+			this.angle.push(angle);
+
+			// Debug
+			var point = document.createElement('div');
+			x$(point)
+				.addClass("point")
+				.addClass("id"+touchObject.identifier)
+				.css({
+					top:newY+"px",
+					left:newX+"px",
+					position:"absolute"
+				})
+				.setStyle("-webkit-transform","rotate("+angle+"deg)")
+				.html("<span>"+angle+"</span>");
+			x$("body").html( "after", point );
+		} else {
+			return false;
+		}
 	}
 }
 
@@ -94,9 +129,8 @@ x$(window).load(function(e){
 		device.type = "unknown"; // Desktop computer or other browser/device
 	}
 	
-	if( window.navigator.standalone ){
+	if( window.navigator.standalone )
 		x$("body").addClass("standalone");
-	}
 
 	updateOrientation();
 	x$(window).orientationchange(updateOrientation);
@@ -105,147 +139,73 @@ x$(window).load(function(e){
 	x$(document)
 	.touchstart( function(e){
 		e.preventDefault();
-		device.touch.start = new Date().getTime();
-		device.touching = true;
-		if( e.touches.length == 1 ){
-			var t1 = e.touches[0];
-
-			device.finger1 = new Finger();
-
-			//// Swipe detection
-			// First info
-			device.finger1.x.push(t1.pageX);
-			device.finger1.y.push(t1.pageY);
-		} else
-		if( e.touches.length == 2 ){
-			var t1 = e.touches[0];
-			var t2 = e.touches[1];
-
-			device.finger1 = new Finger();
-			device.finger2 = new Finger();
-
-			var diffX = t2.pageX-t1.pageX;
-			var diffY = t2.pageY-t1.pageY;
-
-			device.rotate.prev = device.rotate.final; // Old final rotation = previous rotation
-			device.rotate.initial = Math.round( (Math.atan2(diffX,diffY)/(2*Math.PI))*360 ) + 180;
-			// Length of the hypotenuse, given a and b
-			device.pinch.initial =  Math.round( Math.sqrt( diffX*diffX + diffY*diffY ) );
+		log.reset();
+		for( i in e.changedTouches){
+			var t = e.changedTouches[i];
+			if( typeof(t.identifier) != "undefined" ){
+				device.touch[t.identifier] = new Finger(t);
+				log.add("Creating new object: "+t.identifier);
+			}
 		}
 	})
 	.touchmove(function(e){
 		e.preventDefault();
-		if( e.touches.length == 1 ){ // One finger
-			var t1 = e.touches[0];
-			//// Swipe detection
-			var prevX = device.finger1.x[device.finger1.x.length-1];
-			var prevY = device.finger1.y[device.finger1.y.length-1];
-
-			var diffX = prevX-t1.pageX;
-			var diffY = prevY-t1.pageY;
-
-			var delta = Math.round( Math.sqrt( diffX*diffX + diffY*diffY ) );
-			if( delta > 20 ){
-				// Record a new point
-				device.finger1.x.push(t1.pageX);
-				device.finger1.y.push(t1.pageY);
-				var angle = ((Math.floor( ( Math.atan2(diffX,diffY)/Math.PI ) * 180 ) + 180) % 360);
-				var point = "<div class='point' style='position:absolute;top:"+t1.pageY+"px;left:"+t1.pageX+"px;-webkit-transform:rotate(-"+angle+"deg)'><span>&darr;</span></div>";
-				x$("body").html("after",point);
-			}
-
-//			if( !device.swiping && device.swipe.delta > 20 )
-				device.swiping = true;
-
-			if( device.swiping ) {
-				if( device.swipe.angle > 35 && device.swipe.angle < 55 ){ // 45
-					device.swipe.direction = "up-left";
-				} else
-				if( device.swipe.angle > 80 && device.swipe.angle < 100 ){ // 90
-					device.swipe.direction = "left";
-				} else
-				if( device.swipe.angle > 125 && device.swipe.angle < 145 ){ // 135
-					device.swipe.direction = "down-left";
-				} else
-				if( device.swipe.angle > 170 && device.swipe.angle < 190 ){ // 180
-					device.swipe.direction = "down";
-				} else
-				if( device.swipe.angle > 215 && device.swipe.angle < 235 ){ // 225
-					device.swipe.direction = "down-right";
-				} else
-				if( device.swipe.angle > 260 && device.swipe.angle < 280 ){ // 270
-					device.swipe.direction = "right";
-				} else
-				if( device.swipe.angle > 305 && device.swipe.angle < 325 ){ // 315
-					device.swipe.direction = "up-right";
-				} else
-				if( device.swipe.angle > 350 || device.swipe.angle < 10 ){ // 360
-					device.swipe.direction = "up";
-				}
-				event.fire("swipeGesture");
-			}
-		} else
-		if( e.touches.length == 2 ){
-			t1 = e.touches[0]; // Finger 1
-			t2 = e.touches[1]; // Finger 2
-
-			var diffX = t2.pageX - t1.pageX;
-			var diffY = t2.pageY - t1.pageY;
-
-			//// Rotation detection
-			device.rotate.delta = device.rotate.initial - ((Math.floor( ( Math.atan2(diffX,diffY)/Math.PI ) * 180 ) + 180) % 360);
-
-			// The total amount of rotation applied to the object, based on the initial rotate.
-			device.rotate.final = (device.rotate.prev + device.rotate.delta + 360) % 360;
-
-//			if( !device.rotating && Math.abs( device.rotate.delta ) > 10 )
-				device.rotating = true;
-
-			if( device.rotating )
-				event.fire("rotateGesture");
-
-			//// Pinch detection
-			device.pinch.final = Math.floor( Math.sqrt( diffX*diffX + diffY*diffY ) );
-			device.pinch.delta = device.pinch.final - device.pinch.initial;
-			device.pinch.angle = device.rotate.initial;
-
-//			if( !device.pinching && Math.abs( device.pinch.delta ) > 40 )
-				device.pinching = true;
-
-			if( device.pinching )
-				event.fire("pinchGesture");
-		}
+		for( i in device.touch )
+			device.touch[i].addPoint();
 	})
-	.touchend(function(){
-		x$(".point").remove();
-		device.touching = false;
-		device.rotating = false;
-		device.swiping = false;
-		device.swipe.direction = "";
-
-		device.finger1 = false;
-		device.finger2 = false;
+	.touchend(function(e){
+		for( i in e.changedTouches ){
+			var id = e.changedTouches[i].identifier;
+			if( typeof(id) != "undefined" ){
+				x$(".point.id"+id).remove();
+				device.touch.splice(id, 1);
+				log.add("Destroying object: "+id);
+			}
+		}
+		if( e.touches.length == 0 ){
+			// Destroy everything!!!
+			device.touch = [];
+		}
 	});
 });
 
-function rotateSquare() {
-	x$("#square").setStyle("-webkit-transform","rotate("+device.rotate.final+"deg)");
-	x$("#rotate-old span").html(device.rotate.prev+"");
-	x$("#rotate-delta span").html(device.rotate.delta+"");
-	x$("#rotate-initial span").html(device.rotate.initial+"");
-	x$("#rotate-new span").html(device.rotate.final+"");
+// Extra
+/*
+
+// touchstart
+device.rotate.initial = Math.round( (Math.atan2(diffX,diffY)/(2*Math.PI))*360 ) + 180;
+device.pinch.initial =  Math.round( Math.sqrt( diffX*diffX + diffY*diffY ) );
+
+// touchmove
+device.rotate.delta = device.rotate.initial - ((Math.floor( ( Math.atan2(diffX,diffY)/Math.PI ) * 180 ) + 180) % 360);
+device.rotate.final = (device.rotate.prev + device.rotate.delta + 360) % 360;
+device.pinch.final = Math.floor( Math.sqrt( diffX*diffX + diffY*diffY ) );
+device.pinch.delta = device.pinch.final - device.pinch.initial;
+device.pinch.angle = device.rotate.initial;
+
+if( device.swipe.angle > 35 && device.swipe.angle < 55 ){ // 45
+	device.swipe.direction = "up-left";
+} else
+if( device.swipe.angle > 80 && device.swipe.angle < 100 ){ // 90
+	device.swipe.direction = "left";
+} else
+if( device.swipe.angle > 125 && device.swipe.angle < 145 ){ // 135
+	device.swipe.direction = "down-left";
+} else
+if( device.swipe.angle > 170 && device.swipe.angle < 190 ){ // 180
+	device.swipe.direction = "down";
+} else
+if( device.swipe.angle > 215 && device.swipe.angle < 235 ){ // 225
+	device.swipe.direction = "down-right";
+} else
+if( device.swipe.angle > 260 && device.swipe.angle < 280 ){ // 270
+	device.swipe.direction = "right";
+} else
+if( device.swipe.angle > 305 && device.swipe.angle < 325 ){ // 315
+	device.swipe.direction = "up-right";
+} else
+if( device.swipe.angle > 350 || device.swipe.angle < 10 ){ // 360
+	device.swipe.direction = "up";
 }
 
-function pinchSquare() {
-	x$("#pinch-initial span").html(device.pinch.initial+"");
-	x$("#pinch-delta span").html(device.pinch.delta+"");
-}
-
-function swipeSquare() {
-	x$("#swipe-vector span").html(device.swipe.delta+"px, "+device.swipe.angle+"");
-	x$("#swipe-direction span").html(device.swipe.direction+"");
-}
-
-event.listen("rotateGesture", rotateSquare);
-event.listen("pinchGesture", pinchSquare);
-event.listen("swipeGesture", swipeSquare);
+*/
